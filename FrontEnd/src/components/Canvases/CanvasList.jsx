509c42137866ff92123478c4004e9423f7125256
Ravi <ravi@Ravis-MacBook-Air.local>
2025-05-26 19:40:14 +0530

    Added loader

--- FrontEnd/src/components/Canvases/CanvasList.jsx
+++ FrontEnd/src/components/Canvases/CanvasList.jsx
@@ -7,101 +7,114 @@ const CanvasList = ({ canvases }) => {
   const navigate = useNavigate();
   const [email, setEmail] = useState("");
   const [openInput, setOpenInput] = useState(false);
-
+  const [loading, setLoading] = useState(false);
   const getCanvasUrl = `${import.meta.env.VITE_BACKENDURL}/canvas/load/`;
 
   const openCanvas = async (canvasId) => {
     try {
-        const Token = localStorage.getItem("accessToken");
-        console.log("Token:", Token);
-        console.log("Canvas ID:", canvasId);
-        const response = await axios.get(getCanvasUrl + canvasId, {
-            headers: {
-                Authorization: `Bearer ${Token}`,
-            },
-            withCredentials: true,
-        });
+      setLoading(true);
+      const Token = localStorage.getItem("accessToken");
+      // console.log("Token:", Token);
+      // console.log("Canvas ID:", canvasId);
+      const response = await axios.get(getCanvasUrl + canvasId, {
+        headers: {
+          Authorization: `Bearer ${Token}`,
+        },
+        withCredentials: true,
+      });
 
-        console.log("Fetched canvas elements:", response.data.data.elements);
+      // console.log("Fetched canvas elements:", response.data.data.elements);
 
-        // Store the elements in local storage
-        localStorage.setItem("element", JSON.stringify(response.data.data.elements));
-        // Navigate to the canvas page
-        navigate(`/canvas/${canvasId}`);
+      // Store the elements in local storage
+      localStorage.setItem("element", JSON.stringify(response.data.data.elements));
+      setLoading(false);
+      // Navigate to the canvas page
+      navigate(`/canvas/${canvasId}`);
     } catch (error) {
-        console.error("Error fetching canvas:", error);
-        alert("Failed to load canvas. Please try again.");
+      // console.error("Error fetching canvas:", error);
+      alert("Failed to load canvas. Please try again.");
+      setLoading(false);
     }
-};
+  };
 
-const openCanvasToEdit = async (canvasId) => {
+  const openCanvasToEdit = async (canvasId) => {
     try {
-        await openCanvas(canvasId);
-        // Navigate to the edit page
-        navigate(`/edit/${canvasId}`);
+      setLoading(true);
+      await openCanvas(canvasId);
+      // Navigate to the edit page
+      navigate(`/edit/${canvasId}`);
     } catch (error) {
-        console.error("Error opening canvas for editing:", error);
-        alert("Failed to open canvas for editing.");
+      console.error("Error opening canvas for editing:", error);
+      alert("Failed to open canvas for editing.");
+      setLoading(false);
     }
-};
+  };
 
-const deleteCanvasUrl = `${import.meta.env.VITE_BACKENDURL}/canvas/`;
+  const deleteCanvasUrl = `${import.meta.env.VITE_BACKENDURL}/canvas/`;
 
-const deleteCanvas = async (canvasId) => {
+  const deleteCanvas = async (canvasId) => {
     try {
-        const Token = localStorage.getItem("accessToken");
-        const response = await axios.delete(deleteCanvasUrl + canvasId, {
-            headers: {
-                Authorization: `Bearer ${Token}`,
-            },
-            withCredentials: true,
-        });
+      setLoading(true);
+      const Token = localStorage.getItem("accessToken");
+      const response = await axios.delete(deleteCanvasUrl + canvasId, {
+        headers: {
+          Authorization: `Bearer ${Token}`,
+        },
+        withCredentials: true,
+      });
 
-        console.log("Canvas deleted successfully:", response.data);
-        alert("Canvas deleted successfully!");
-        window.location.reload();
+      console.log("Canvas deleted successfully:", response.data);
+      alert("Canvas deleted successfully!");
+      setLoading(false);
+      window.location.reload();
     } catch (error) {
-        console.error("Error deleting canvas:", error);
-        alert("Failed to delete canvas. Please try again.");
+      console.error("Error deleting canvas:", error);
+      alert("Failed to delete canvas. Please try again.");
+      setLoading(false);
     }
-};
+  };
 
-const handleInputEvent = () =>{
+  const handleInputEvent = () => {
     setOpenInput(!openInput);
-}
+  }
 
-const setCanvasIdToLocalStorage = (canvasId) => {
+  const setCanvasIdToLocalStorage = (canvasId) => {
     localStorage.setItem("canvasId", canvasId);
     handleInputEvent();
-}
+  }
 
-const shareCanvas = async () => {
+  const shareCanvas = async () => {
     try {
-      handleInputEvent();
-      const canvasId = localStorage.getItem("canvasId");
-        const Token = localStorage.getItem("accessToken");
-        const response = await axios.put(
-            `${import.meta.env.VITE_BACKENDURL}/canvas/share/${canvasId}`,
-            {email},
-            {
-                headers: {
-                    Authorization: `Bearer ${Token}`,
-                },
-                withCredentials: true,
-            }
-        );
 
-        console.log("Canvas shared successfully:", response.data);
-        alert("Canvas shared successfully!");
+      handleInputEvent();
+      setLoading(true);
+      const canvasId = localStorage.getItem("canvasId");
+      const Token = localStorage.getItem("accessToken");
+
+      const response = await axios.put(
+        `${import.meta.env.VITE_BACKENDURL}/canvas/share/${canvasId}`,
+        { email },
+        {
+          headers: {
+            Authorization: `Bearer ${Token}`,
+          },
+          withCredentials: true,
+        }
+      );
+
+      console.log("Canvas shared successfully:", response.data);
+      setLoading(false);
+      alert("Canvas shared successfully!");
     } catch (error) {
-        console.error("Error sharing canvas:", error);
-        alert("Failed to share canvas. Please try again.");
+      console.error("Error sharing canvas:", error);
+      alert("Failed to share canvas. Please try again.");
+      setLoading(false);
     }
-}
+  }
 
   return (
     <div className="w-10/12 p-2">
-       {openInput && (
+      {openInput && (
         <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white p-4 rounded-md shadow-xl z-50 flex flex-col gap-4 justify-center items-center">
           <h2 className="text-xl font-semibold">Save your canvas</h2>
           <div className="flex gap-4">
@@ -115,48 +128,58 @@ const shareCanvas = async () => {
             <button onClick={shareCanvas} className="bg-orange-500 text-white p-2 rounded-md">
               Send
             </button>
+            <button onClick={()=>{setOpenInput(!openInput)}} className="bg-orange-500 text-white p-2 rounded-md">
+              Cancel
+            </button>
           </div>
         </div>
       )}
       <h2 className="md:text-5xl text-2xl w-full overflow-y-hidden text-center font-semibold lg:mb-4 text-white">Your Canvas</h2>
-      <div className="mt-16 w-full flex flex-col md:flex-row justify-evenly items-center md:flex-wrap gap-4">
-        {canvases.length > 0 ? (
-          canvases?.map((canvas) => (
-            <div
-              key={canvas._id}
-              className="md:p-4 bg-white shadow-3xl w-full md:w-3/12 min-h-64 rounded-lg border flex flex-col justify-evenly border-gray-200"
-            >
+      {
+        (loading === true) ? (
+          <div className="flex justify-center items-center h-64">
+            <p className="text-white">Loading...</p>
+          </div>
+        ) :
+          <div className="mt-16 w-full flex flex-col md:flex-row justify-evenly items-center md:flex-wrap gap-4">
+            {canvases.length > 0 ? (
+              canvases?.map((canvas) => (
+                <div
+                  key={canvas._id}
+                  className="md:p-4 bg-white shadow-3xl w-full md:w-3/12 min-h-64 rounded-lg border flex flex-col justify-evenly border-gray-200"
+                >
 
 
-              <div className="mx-auto flex flex-col items-center">
-                <h3 className="font-bold text-2xl leading-10 text-gray-800">{canvas.name}</h3>
-                <p className="text-lg text-gray-700 leading-10">
-                  Created At: {new Date(canvas.createdAt).toLocaleString()}
-                </p>
-              </div>
+                  <div className="mx-auto flex flex-col items-center">
+                    <h3 className="font-bold text-2xl leading-10 text-gray-800">{canvas.name}</h3>
+                    <p className="text-lg text-gray-700 leading-10">
+                      Created At: {new Date(canvas.createdAt).toLocaleString()}
+                    </p>
+                  </div>
 
 
-              <div className="flex gap-3 justify-center items-center flex-wrap">
-                <button onClick={() => openCanvas(canvas._id)} className="bg-blueNew text-white p-2 rounded-md mt-2">
-                  Open
-                </button>
-                <button onClick={() => openCanvasToEdit(canvas._id)} className="bg-blueNew text-white p-2 rounded-md mt-2">
-                  Edit
-                </button>
-                <button onClick={() => deleteCanvas(canvas._id)} className="bg-blueNew text-white p-2 rounded-md mt-2">
-                  Delete
-                </button>
-                <button onClick={() => setCanvasIdToLocalStorage(canvas._id)} className="bg-blueNew text-white p-2 rounded-md mt-2">
-                  Share
-                </button>
-              </div>
+                  <div className="flex gap-3 justify-center items-center flex-wrap">
+                    <button onClick={() => openCanvas(canvas._id)} className="bg-blueNew text-white p-2 rounded-md mt-2">
+                      Open
+                    </button>
+                    <button onClick={() => openCanvasToEdit(canvas._id)} className="bg-blueNew text-white p-2 rounded-md mt-2">
+                      Edit
+                    </button>
+                    <button onClick={() => deleteCanvas(canvas._id)} className="bg-blueNew text-white p-2 rounded-md mt-2">
+                      Delete
+                    </button>
+                    <button onClick={() => setCanvasIdToLocalStorage(canvas._id)} className="bg-blueNew text-white p-2 rounded-md mt-2">
+                      Share
+                    </button>
+                  </div>
 
-            </div>
-          ))
-        ) : (
-          <p className="text-white">No canvases available</p>
-        )}
-      </div>
+                </div>
+              ))
+            ) : (
+              <p className="text-white">No canvases available</p>
+            )}
+          </div>
+      }
     </div>
   );
 };
--- FrontEnd/src/components/Canvases/SharedCanvasList.jsx
+++ FrontEnd/src/components/Canvases/SharedCanvasList.jsx
@@ -1,15 +1,18 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
+
 function SharedCanvasList({ canvases }) {
   const [canvasDetails, setCanvasDetails] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     const fetchSharedUsers = async () => {
+      setLoading(true);
       const Token = localStorage.getItem("accessToken");
       const updatedCanvasDetails = await Promise.all(
         canvases
-          .filter((canvas) => canvas.shared.length > 0) 
+          .filter((canvas) => canvas.shared.length > 0)
           .map(async (canvas) => {
             const sharedUsers = await Promise.all(
               canvas.shared.map(async (userId) => {
@@ -22,17 +25,19 @@ function SharedCanvasList({ canvases }) {
                       withCredentials: true,
                     }
                   );
-                  return response.data.data; 
+                  setLoading(false);
+                  return response.data.data;
                 } catch (error) {
                   console.error("Error fetching user:", error);
-                  return null; 
+                  setLoading(false);
+                  return null;
                 }
               })
             );
 
             return {
               ...canvas,
-              sharedUsers: sharedUsers.filter((user) => user !== null), 
+              sharedUsers: sharedUsers.filter((user) => user !== null),
             };
           })
       );
@@ -46,46 +51,53 @@ function SharedCanvasList({ canvases }) {
   return (
     <div className="w-10/12 mx-auto p-2 min-h-screen ">
       <h2 className="text-3xl lg:text-5xl overflow-y-hidden text-center font-semibold m-8 text-white">Shared Canvas</h2>
-      <div className=" w-full flex flex-col md:flex-row md:justify-evenly items-center md:flex-wrap gap-4">
-        {canvasDetails.length > 0 ? (
-          canvasDetails.map((canvas) => (
-            <div
-              key={canvas._id}
-              className="p-4 bg-white w-full lg:w-10/12 shadow-md rounded-lg border border-gray-200"
-            >
-              <h3 className="text-xl font-bold text-gray-800">
-                {canvas.name}
-              </h3>
-              <p className="text-sm text-gray-500">
-                Created At: {new Date(canvas.createdAt).toLocaleString()}
-              </p>
-              <h4 className="mt-2 text-lg font-semibold text-gray-700">
-                Shared With
-              </h4>
-              {canvas.sharedUsers.length > 0 ? (
-                <ul className="list-disc ml-4 text-gray-600 text-sm">
-                  {canvas.sharedUsers.map((user) => (
-                    <li key={user._id} className="">
-                      <div className="flex justify-between font-semibold text-md leading-6">
-                        <div>
-                          {user.username}
-                        </div>
-                        <div>
-                          {user.email}
-                        </div>
-                      </div>
-                    </li>
-                  ))}
-                </ul>
-              ) : (
-                <p className="text-gray-500">No users found</p>
-              )}
-            </div>
-          ))
-        ) : (
-          <p className="text-gray-500">No shared canvases available</p>
-        )}
-      </div>
+      {
+        (loading === true) ? (
+          <div className="flex justify-center items-center h-64">
+            <p className="text-white">Loading...</p>
+          </div>
+        ) :
+          <div className=" w-full flex flex-col md:flex-row md:justify-evenly items-center md:flex-wrap gap-4">
+            {canvasDetails.length > 0 ? (
+              canvasDetails.map((canvas) => (
+                <div
+                  key={canvas._id}
+                  className="p-4 bg-white w-full lg:w-10/12 shadow-md rounded-lg border border-gray-200"
+                >
+                  <h3 className="text-xl font-bold text-gray-800">
+                    {canvas.name}
+                  </h3>
+                  <p className="text-sm text-gray-500">
+                    Created At: {new Date(canvas.createdAt).toLocaleString()}
+                  </p>
+                  <h4 className="mt-2 text-lg font-semibold text-gray-700">
+                    Shared With
+                  </h4>
+                  {canvas.sharedUsers.length > 0 ? (
+                    <ul className="list-disc ml-4 text-gray-600 text-sm">
+                      {canvas.sharedUsers.map((user) => (
+                        <li key={user._id} className="">
+                          <div className="flex justify-between font-semibold text-md leading-6">
+                            <div>
+                              {user.username}
+                            </div>
+                            <div>
+                              {user.email}
+                            </div>
+                          </div>
+                        </li>
+                      ))}
+                    </ul>
+                  ) : (
+                    <p className="text-gray-500">No users found</p>
+                  )}
+                </div>
+              ))
+            ) : (
+              <p className="text-gray-500">No shared canvases available</p>
+            )}
+          </div>
+      }
     </div>
   );
 }
